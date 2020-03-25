2cdb0e0e332a97791a71e67db62b9f099b9800fa
dobashiyuuki <dobyuk@gmail.com>
2020-03-26 04:52:33 +0900

    200326

--- nn/core.py
+++ nn/core.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-from . import helpers
+# from . import helpers
+from sklearn.datasets import load_iris
 
 def get_hmm():
     """Get a thought."""
@@ -10,3 +11,7 @@ def hmm():
     """Contemplation..."""
     if helpers.get_answer():
         print(get_hmm())
+
+# load datasets
+iris_data = load_iris()
+print(iris_data)
